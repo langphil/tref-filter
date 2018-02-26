32afa482c4957b5d033ba10f6aceec85f45b59ad
Phil Lang <plangphoto@gmail.com>
2018-02-26 09:21:56 +0000

    Refactoring

--- tref.py
+++ tref.py
@@ -1,32 +1,40 @@
 import pandas as pd
 import glob
 
-keywords = ['abortion']
+keywords = ['referendum']
+files = []
+df_concat = []
+df_full = []
 df_filtered = []
 
 # Opening multiple CSV's
-files = glob.glob("./input/*.csv")
-df_list = []
+def open_file():
+	files.extend(glob.glob("./input/*.csv"))
 
 # Concat multiple CSV's into a single dataframe
-for filename in sorted(files):
-	df_list.append(pd.read_csv(filename))
-
-df_full = pd.concat(df_list)
+def concat_files():
+	for filename in sorted(files):
+		df_concat.append(pd.read_csv(filename))
+	df_full.extend(pd.concat(df_concat))
 
 # Filter for keywords in the text column
-for kw in keywords:
-	df_full.text = df_full.text.str.lower()
-	filtered = df_full.loc[df_full['text'].str.contains(kw, na=False)]
-	df_filtered.append(filtered)
-
-# Concatenate the array of datatables
-df_res = pd.concat(df_filtered)
+def keyword_filter():
+	print df_full
+	for kw in keywords:
+		df_full.text = df_full.text.str.lower()
+		filtered = df_full.loc[df_full['text'].str.contains(kw, na=False)]
+		df_filtered.append(filtered)
 
 # Drop duplicate rows and unnecessary columns
-df_res.drop_duplicates(subset=["text"], inplace=True)
-df_res.drop(df_res.columns[[0]], inplace=True, axis=1)
+def datatable_clean():
+	df_res.drop_duplicates(subset=["text"], inplace=True)
+	df_res.drop(df_res.columns[[0]], inplace=True, axis=1)
 
-# Save filtered dataframe to a CSV
+# Process
+open_file()
+concat_files()
+keyword_filter()
+df_res = pd.concat(df_filtered)
+datatable_clean()
 print "CSV's filtered and saved to the output folder"
 df_res.to_csv('./output/output.csv',  index = False)
